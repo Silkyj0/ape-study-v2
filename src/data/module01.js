e6b463eb5474c71f69d8e1fc7b01e70b12dd2ae9
Silkyj0 <124425427+Silkyj0@users.noreply.github.com>
2026-09-04 23:12:38 +1000

    Add Module 1 question bank index

---
+++ src/data/module01.js
@@ -0,0 +1,15 @@
+import { module01Part01 } from './module01.part01.js';
+import { module01Part02 } from './module01.part02.js';
+import { module01Part03 } from './module01.part03.js';
+import { module01Part04 } from './module01.part04.js';
+import { module01Part05 } from './module01.part05.js';
+import { module01Part06 } from './module01.part06.js';
+
+export const module01Questions = [
+  ...module01Part01,
+  ...module01Part02,
+  ...module01Part03,
+  ...module01Part04,
+  ...module01Part05,
+  ...module01Part06
+];
