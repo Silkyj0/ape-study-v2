91558ae02511c63d8c00cc99153b39245fcabbe3
Silkyj0 <124425427+Silkyj0@users.noreply.github.com>
2026-09-04 23:10:13 +1000

    Add question-bank index

---
+++ src/data/questions.js
@@ -0,0 +1,27 @@
+import { module01Questions } from './module01.js';
+import { module02Questions } from './module02.js';
+import { module03Questions } from './module03.js';
+import { module04Questions } from './module04.js';
+import { module05Questions } from './module05.js';
+import { module06Questions } from './module06.js';
+import { module07Questions } from './module07.js';
+import { module08Questions } from './module08.js';
+import { module09Questions } from './module09.js';
+import { module10Questions } from './module10.js';
+import { module11Questions } from './module11.js';
+
+export const SEED_VERSION = 14;
+
+export const SEED = [
+  ...module01Questions,
+  ...module02Questions,
+  ...module03Questions,
+  ...module04Questions,
+  ...module05Questions,
+  ...module06Questions,
+  ...module07Questions,
+  ...module08Questions,
+  ...module09Questions,
+  ...module10Questions,
+  ...module11Questions,
+];
