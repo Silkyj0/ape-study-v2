dd7911a302b4eca91e8bb499dffbcf09b1352321
Silkyj0 <124425427+Silkyj0@users.noreply.github.com>
2026-09-04 23:12:48 +1000

    Add Module 2 question bank index

---
+++ src/data/module02.js
@@ -0,0 +1,37 @@
+import { module02Part01 } from './module02.part01.js';
+import { module02Part02 } from './module02.part02.js';
+import { module02Part03 } from './module02.part03.js';
+import { module02Part04 } from './module02.part04.js';
+import { module02Part05 } from './module02.part05.js';
+import { module02Part06 } from './module02.part06.js';
+import { module02Part07 } from './module02.part07.js';
+import { module02Part08 } from './module02.part08.js';
+import { module02Part09 } from './module02.part09.js';
+import { module02Part10 } from './module02.part10.js';
+import { module02Part11 } from './module02.part11.js';
+import { module02Part12 } from './module02.part12.js';
+import { module02Part13 } from './module02.part13.js';
+import { module02Part14 } from './module02.part14.js';
+import { module02Part15 } from './module02.part15.js';
+import { module02Part16 } from './module02.part16.js';
+import { module02Part17 } from './module02.part17.js';
+
+export const module02Questions = [
+  ...module02Part01,
+  ...module02Part02,
+  ...module02Part03,
+  ...module02Part04,
+  ...module02Part05,
+  ...module02Part06,
+  ...module02Part07,
+  ...module02Part08,
+  ...module02Part09,
+  ...module02Part10,
+  ...module02Part11,
+  ...module02Part12,
+  ...module02Part13,
+  ...module02Part14,
+  ...module02Part15,
+  ...module02Part16,
+  ...module02Part17
+];
