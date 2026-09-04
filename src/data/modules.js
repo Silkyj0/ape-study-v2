7a26d9122fe92fd1c04ba43a15a516b2b86bda9a
Silkyj0 <124425427+Silkyj0@users.noreply.github.com>
2026-09-04 23:10:06 +1000

    Add module definitions

---
+++ src/data/modules.js
@@ -0,0 +1,46 @@
+export const MODULES = [
+  {
+    "id": 1,
+    "title": "Architects, the Building Industry and Professional Liability"
+  },
+  {
+    "id": 2,
+    "title": "Setting up an Architectural Practice, Copyright and Moral Rights"
+  },
+  {
+    "id": 3,
+    "title": "Commissions, Service Provision, Fees + Agreements (Contracts)"
+  },
+  {
+    "id": 4,
+    "title": "Project Fundamentals, Planning & Consultants"
+  },
+  {
+    "id": 5,
+    "title": "Responsible Design - Country, Environment, Equity, Safety, Amenity"
+  },
+  {
+    "id": 6,
+    "title": "Building Procurement, Construction Contracts, Construction Documentation"
+  },
+  {
+    "id": 7,
+    "title": "Tendering and Construction Contract Execution"
+  },
+  {
+    "id": 8,
+    "title": "Contract Administration - Construction Commencement"
+  },
+  {
+    "id": 9,
+    "title": "Contract Administration - Money"
+  },
+  {
+    "id": 10,
+    "title": "Contract Administration - Time, Time Extension Costs, Liquidated Damages"
+  },
+  {
+    "id": 11,
+    "title": "Contract Administration - Final Certificate, Dispute Resolution + Termination"
+  }
+];
