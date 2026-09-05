1681d87939f0e983e9ba80ec149a93d205f9030b
Silkyj0 <124425427+Silkyj0@users.noreply.github.com>
2026-09-05 13:36:14 +1000

    Include locked Module 4 PARCS sample set

--- src/data/module04.js
+++ src/data/module04.js
@@ -5,6 +5,7 @@ import { module04Part04 } from './module04.part04.js';
 import { module04Part05 } from './module04.part05.js';
 import { module04Part06 } from './module04.part06.js';
 import { module04Part07 } from './module04.part07.js';
+import { module04ParcsQuestions } from './module04.parcs.js';
 
 export const module04Questions = [
   ...module04Part01,
@@ -14,6 +15,7 @@ export const module04Questions = [
   ...module04Part05,
   ...module04Part06,
   ...module04Part07,
+  ...module04ParcsQuestions,
 ];
 
 export const module04Ids = module04Questions.map((question) => question.id);
