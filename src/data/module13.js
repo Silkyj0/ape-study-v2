8ff605d7cf3910370e80d33f455a5cb832e1ef74
Silkyj0 <124425427+Silkyj0@users.noreply.github.com>
2026-09-10 09:21:38 +1000

    Add supplementary CAA2024 module

---
+++ src/data/module13.js
@@ -0,0 +1,4 @@
+import { CAA2024_QUESTIONS } from './caa2024/questions.js';
+
+export const module13Questions = [...CAA2024_QUESTIONS];
+export const module13Ids = module13Questions.map((question) => question.id);
