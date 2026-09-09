c462bbbbfffad346830baf286a8d0b3bb053675d
Silkyj0 <124425427+Silkyj0@users.noreply.github.com>
2026-09-10 09:26:55 +1000

    Complete CAA2024 M13 flashcard assembly

--- src/data/flashcards/module13.js
+++ src/data/flashcards/module13.js
@@ -1,3 +1,7 @@
 import { CAA2024_FLASHCARDS } from '../caa2024/flashcards.js';
+import { CAA2024_ITEM_FLASHCARDS } from '../caa2024/flashcardsItems.js';
 
-export const FLASHCARDS_M13 = [...CAA2024_FLASHCARDS];
+export const FLASHCARDS_M13 = [
+  ...CAA2024_FLASHCARDS,
+  ...CAA2024_ITEM_FLASHCARDS,
+];
